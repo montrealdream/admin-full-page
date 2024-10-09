d3c2cc50e019e3c17dd00fe8b248539dfe155237
coder-dom <giangtruong100702@gmail.com>
2024-10-09 10:45:43 +0700

    form-search tìm kiếm

--- js/script.js
+++ js/script.js
@@ -23,3 +23,29 @@ if(buttonFilterStatus) {
     });
 }
 // HẾT BỘ LỌC TRẠNG THÁI - End Filter Button Status
+
+// FORM TÌM KIẾM - Form Search
+const formSearch = document.querySelector("[form-search]");
+if(formSearch) {
+    // lắng nge sự kiện
+    formSearch.addEventListener("submit", (event) => {
+        event.preventDefault(); // chặn sự kiện mặc định của form
+
+        // lấy đường dẫn url - Bom Location
+        let url = new URL(location.href); 
+
+        let tagInput = formSearch.querySelector("input");
+
+        // set ?query cho url
+        if(tagInput.value)
+            url.searchParams.set("keyword", tagInput.value);
+
+        else
+            url.searchParams.delete("keyword");
+        
+        // chuyển hướng url
+        window.location.href = url.href;
+    });
+}
+// END FORM TÌM KIẾM - Hết Form Search
+
